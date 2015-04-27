bb7aca9a5267c9a76326319f258923f458e0ffe4
Jimmy.Chen <greenmoach@gmail.com>
2015-04-28 02:00:33 +0800

    1. Add ejs layout with test page index.ejs

--- cloud/app.js
+++ cloud/app.js
@@ -10,7 +10,11 @@ app.use(express.bodyParser());    // Middleware for reading request body
 // This is an example of hooking up a request handler with a specific request
 // path and HTTP verb using the Express routing API.
 app.get('/hello', function(req, res) {
-  res.render('hello', { message: 'Congrats, you just set up your app!' });
+  res.render('hello', { message: 'Congrats, you just set up your apps!' });
+});
+
+app.get('/index', function(req, res){
+    res.render('pages/index');
 });
 
 // // Example reading from the request query string of an HTTP get request.
